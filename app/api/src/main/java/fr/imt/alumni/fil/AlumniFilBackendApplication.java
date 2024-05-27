156629b9b8b26147ee0da046e871cab2cccbcb6f
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-05-27 14:55:41 +0200

    init: initialize spring boot app

---
+++ app/api/src/main/java/fr/imt/alumni/fil/AlumniFilBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.imt.alumni.fil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniFilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniFilBackendApplication.class, args);
+	}
+
+}
---
+++ app/api/src/test/java/fr/imt/alumni/fil/AlumniFilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.imt.alumni.fil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniFilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
