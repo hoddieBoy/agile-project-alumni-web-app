a2e332118cd0f2b27b2138db3c8e048127065f6e
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-05-29 15:39:48 +0200

    feat(handler): update global exception handler
    - Enhanced `ControllerExceptionHandler` to handle validation exceptions.
    - Added method to return validation errors with `400 Bad Request`.

--- app/api/src/main/java/fr/imt/alumni/fil/controller/ControllerExceptionHandler.java
+++ app/api/src/main/java/fr/imt/alumni/fil/controller/ControllerExceptionHandler.java
@@ -2,19 +2,33 @@ package fr.imt.alumni.fil.controller;
 
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleException(Exception e) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of("error", e.getMessage()));
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleException(Exception e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return Map.of(
+                "message", "Some fiels doesn't respect the constraints",
+                "errors", errors
+        );
     }
 }
