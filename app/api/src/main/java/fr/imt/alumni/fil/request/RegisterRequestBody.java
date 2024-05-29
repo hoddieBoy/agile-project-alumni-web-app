ead165b8f4d3af824571c86128fa2d5a1d22f6ed
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-05-29 15:41:32 +0200

    chore(validation): add common request body interface
    - Created `RequestBody` interface for common request body validation.

---
+++ app/api/src/main/java/fr/imt/alumni/fil/request/RegisterRequestBody.java
@@ -0,0 +1,17 @@
+package fr.imt.alumni.fil.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequestBody(
+        @NotBlank(message = "username is mandatory")
+        @Size(min = 3, max = 20, message = "username must be between 3 and 20 characters")
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "username must contain only letters and numbers")
+        String username,
+        @NotBlank(message = "password is mandatory")
+        @Size(min = 8, max = 20, message = "password must be between 8 and 20 characters")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]*$", message = "password must contain at least one uppercase letter, one lowercase letter and one number")
+        String password
+) implements RequestBody {
+}
