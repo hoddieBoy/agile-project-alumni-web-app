f3d4ad9c3db84b76d6d5443b1256a83d920a8555
ANAS ALAOUI <anas.alaoui@laposte.fr>
2024-05-29 15:02:14 +0200

    Add of Statistic Methods/Requests

--- app/api/src/main/resources/db/migration/V1__create_alumi.sql
+++ app/api/src/main/resources/db/migration/V1__create_alumi.sql
@@ -1,13 +1,15 @@
 CREATE TABLE alumni
 (
-    id         UUID NOT NULL,
-    first_name VARCHAR(255),
-    last_name  VARCHAR(255),
-    age        VARCHAR(255),
-    sex        SMALLINT,
-    company    VARCHAR(255),
-    country    VARCHAR(255),
-    city       VARCHAR(255),
+    id  UUID NOT NULL,
+    first_name  VARCHAR(255),
+    last_name   VARCHAR(255),
+    mail    VARCHAR(255),
+    sex SMALLINT,
+    coop_company    VARCHAR(255),
+    curent_company  VARCHAR(255),
+    website VARCHAR(255),
+    country VARCHAR(255),
+    city    VARCHAR(255),
     is_stayed  BOOLEAN,
 
     CONSTRAINT pk_alumni PRIMARY KEY (id)
