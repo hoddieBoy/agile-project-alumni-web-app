2b9ec64bc844c8de47811ea29159c5550040d76e
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-05-30 20:28:47 +0200

    chore(controller): update controllers for security
    - Added SecurityRequirement annotations to AlumniController and PromotionController.

--- app/api/src/main/java/fr/imt/alumni/fil/controller/PromotionController.java
+++ app/api/src/main/java/fr/imt/alumni/fil/controller/PromotionController.java
@@ -1,15 +1,15 @@
 package fr.imt.alumni.fil.controller;
 
-import java.util.List;
-
+import fr.imt.alumni.fil.service.StatisticService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import fr.imt.alumni.fil.service.StatisticService;
 
 @RestController
 @RequestMapping("/api/v1/alumni-fil/statistic")
+@SecurityRequirement(name = "Bearer Authentification")
 public class PromotionController {
 
     @Autowired
