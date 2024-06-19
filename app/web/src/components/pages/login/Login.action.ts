dcb698534ebe310928736b2de391867c8546ab38
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-06-19 14:12:59 +0200

    feat(app/web): add login action and authentication response types
    - Created `Login.action.ts` in `src/components/pages/login/` to handle login actions.
    - Created `AuthenticateResponse.ts` in `src/payload/response/` to define the structure of authentication responses.
    
    These changes introduce login action handling and define the structure for authentication responses in the application.

---
+++ app/web/src/components/pages/login/Login.action.ts
@@ -0,0 +1,25 @@
+import { ActionFunctionArgs, redirect, json } from "react-router-dom";
+import axiosConfig from "config/axiosConfig";
+import { AuthenticateResponse } from "payload/response/AuthenticateResponse";
+
+export async function action({ request }: ActionFunctionArgs<'post'>) {
+    const formData = await request.formData();
+    const payload = Object.fromEntries(formData);
+
+    if (!payload.username || !payload.password) {
+        return json({ message: 'Username and Password are required.' }, { status: 400 });
+    }
+
+    try {
+        const response = await axiosConfig.post<AuthenticateResponse>('/auth/authenticate', payload);
+        const data = response.data;
+        const accessTokenExpiry = new Date(Date.now() + 1000 * 60 * 60); // 1 hour
+        const refreshTokenExpiry = new Date(Date.now() + 1000 * 60 * 60 * 24); // 24 hours
+
+        document.cookie = `access_token=${data.access_token}; expires=${accessTokenExpiry.toUTCString()}; path=/`;
+        document.cookie = `refresh_token=${data.refresh_token}; expires=${refreshTokenExpiry.toUTCString()}; path=/`;
+        return redirect('/');
+    } catch (error) {
+        return json({ message: 'Invalid username or password.' }, { status: 401 });
+    }
+}
---
+++ app/web/src/payload/response/AuthenticateResponse.ts
@@ -0,0 +1,8 @@
+export interface AuthenticateResponse {
+    user_id: string;
+    username: string;
+    roles: string[];
+    access_token: string;
+    refresh_token: string;
+    token_type: string;
+}
