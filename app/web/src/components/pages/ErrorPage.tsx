695c77b0ca1f68de1cffb67cceac3cfc8988af05
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-06-23 14:09:07 +0200

    refactor(app/web): refactor ErrorPage component
    - Refactored `ErrorPage.tsx` to handle different error statuses more cleanly.
    - Added comments for better code readability.

--- app/web/src/components/pages/ErrorPage.tsx
+++ app/web/src/components/pages/ErrorPage.tsx
@@ -1,40 +1,49 @@
-import React  from 'react';
-import { Button, Result } from 'antd';
-import {useRouteError, isRouteErrorResponse} from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import {Button, Result} from 'antd';
+import {isRouteErrorResponse, useNavigate, useRouteError} from 'react-router-dom';
 
-function ErrorPage() {
+const ErrorPage = () => {
     const error = useRouteError();
     const navigate = useNavigate();
 
+    // Check if the error is a RouteErrorResponse and has a status property
     if (!isRouteErrorResponse(error)) {
         return null;
     }
 
-    if (error.status === 404) {
-        return <Result
-            status="404"
-            title="404"
-            subTitle="Sorry, the page you visited does not exist."
-            extra={<Button type="primary">Back Home</Button>}
-        />;
-    }
+    const handleBackHome = () => navigate('/');
+    const handleLogin = () => navigate('/login');
+    const handleGoBack = () => navigate(-1);
 
-    if (error.status === 403) {
-        return <Result
-            status="403"
-            title="403"
-            subTitle="Sorry, you are not authorized to access this page."
-            extra={<Button type="primary" onClick={() => navigate('/login')}>Login</Button>}
-        />;
+    switch (error.status) {
+        case 404:
+            return (
+                <Result
+                    status="404"
+                    title="404"
+                    subTitle="Sorry, the page you visited does not exist."
+                    extra={<Button type="primary" onClick={handleBackHome}>Back Home</Button>}
+                />
+            );
+        case 403:
+            return (
+                <Result
+                    status="403"
+                    title="403"
+                    subTitle="Sorry, you are not authorized to access this page."
+                    extra={<Button type="primary" onClick={handleLogin}>Login</Button>}
+                />
+            );
+        default:
+            return (
+                <Result
+                    status="500"
+                    title="500"
+                    subTitle="Sorry, something went wrong."
+                    extra={<Button type="primary" onClick={handleGoBack}>Go Back</Button>}
+                />
+            );
     }
-
-    return <Result
-        status="500"
-        title="500"
-        subTitle="Sorry, something went wrong."
-        extra={<Button type="primary" onClick={() => navigate(-1)}>Go Back</Button>}
-    />;
-}
+};
 
 export default ErrorPage;
