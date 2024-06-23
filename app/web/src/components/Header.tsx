324a0cca84b4714b99de501585ce896c2d3e16b9
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-06-23 18:52:34 +0200

    feat(app/web): add Header component
    - Created `Header.tsx` in `src/components/` to display a header.
    - Added `Header.css` for styling the header.

---
+++ app/web/src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {deleteCookie} from "utils/Cookie";
+import {redirect} from "react-router-dom";
+import "components/Header.css";
+
+export default function Header() {
+    const handleLogout = () => {
+        deleteCookie('token');
+        redirect('/login');
+    }
+
+    return (
+        <header className="header">
+            <div className="header-left">
+                <a href="#" className="navbar-brand">
+                    <img
+                        src="https://www.imt-atlantique.fr/sites/default/files/Images/Ecole/charte-graphique/IMT_Atlantique_logo_RVB_Negatif_Baseline_400x272.png"
+                        alt="IMT Atlantique Logo"
+                    />
+                </a>
+                <input type="text" className="form-control navbar-search" placeholder="Search"/>
+            </div>
+            <nav className="header-right">
+                <a href="#" className="nav-link">Search</a>
+                <a href="#" className="nav-link">Import/Export</a>
+                <a href="#" className="nav-link">Gestion Utilisateurs</a>
+                <button className="btn">Log out</button>
+            </nav>
+        </header>
+    );
+}
