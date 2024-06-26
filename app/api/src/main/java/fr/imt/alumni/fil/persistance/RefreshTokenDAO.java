1b9f28c356a1defb53c4d768efb679b87de1aeb6
JanaZebian <146324410+JanaZebian@users.noreply.github.com>
2024-06-26 16:00:06 +0200

    Feat/delete user functionality (#51)

--- app/api/src/main/java/fr/imt/alumni/fil/persistance/RefreshTokenDAO.java
+++ app/api/src/main/java/fr/imt/alumni/fil/persistance/RefreshTokenDAO.java
@@ -1,9 +1,12 @@
 package fr.imt.alumni.fil.persistance;
 
 import fr.imt.alumni.fil.domain.bo.RefreshToken;
+import fr.imt.alumni.fil.domain.bo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface RefreshTokenDAO extends JpaRepository<RefreshToken, UUID> {
+
+    void deleteAllByUser(User user);
 }
---
+++ app/api/src/main/java/fr/imt/alumni/fil/service/UserService.java
@@ -0,0 +1,31 @@
+package fr.imt.alumni.fil.service;
+
+import fr.imt.alumni.fil.persistance.RefreshTokenDAO;
+import fr.imt.alumni.fil.persistance.UserDAO;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserDAO userDAO;
+    private final RefreshTokenDAO refreshTokenDAO;
+
+    public UserService(UserDAO userDAO, RefreshTokenDAO refreshTokenDAO) {
+        this.userDAO = userDAO;
+        this.refreshTokenDAO = refreshTokenDAO;
+    }
+
+    @Transactional
+    public void deleteUSer(String id) {
+        String idTrimed = Optional.ofNullable(id).map(String::trim).orElse("");
+        UUID uuid = UUID.fromString(idTrimed);
+        userDAO.findById(uuid).ifPresent(user -> {
+            refreshTokenDAO.deleteAllByUser(user);
+            userDAO.delete(user);
+        });
+    }
+}
