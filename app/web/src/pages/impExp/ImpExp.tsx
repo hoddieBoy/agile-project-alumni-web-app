ac9bbd775eae3d7710efbfcf7f84c7599e04d06a
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-06-27 22:04:32 +0200

    chore: update css

--- app/web/src/pages/impExp/ImpExp.tsx
+++ app/web/src/pages/impExp/ImpExp.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
-import { getAccessToken } from 'utils/Token';
+import {getAccessToken} from 'utils/Token';
 import './ImpExp.css';
 
 const AlumniImportExport: React.FC = () => {
@@ -17,26 +17,26 @@ const AlumniImportExport: React.FC = () => {
                 'Authorization': `Bearer ${getAccessToken()}`
             },
         })
-        .then(response => {
-            if (response.ok) {
-                return response.blob();
-            } else {
-                throw new Error('Network response was not ok.');
-            }
-        })
-        .then(blob => {
-            const link = document.createElement('a');
-            const url = window.URL.createObjectURL(blob);
-            link.href = url;
-            link.setAttribute('download', `alumni_${selectedYear}.csv`);
-            document.body.appendChild(link);
-            link.click();
-            link.parentNode?.removeChild(link);
-            window.URL.revokeObjectURL(url);
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                } else {
+                    throw new Error('Network response was not ok.');
+                }
+            })
+            .then(blob => {
+                const link = document.createElement('a');
+                const url = window.URL.createObjectURL(blob);
+                link.href = url;
+                link.setAttribute('download', `alumni_${selectedYear}.csv`);
+                document.body.appendChild(link);
+                link.click();
+                link.parentNode?.removeChild(link);
+                window.URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
     };
 
     const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,17 +65,17 @@ const AlumniImportExport: React.FC = () => {
                     'Authorization': `Bearer ${getAccessToken()}`
                 }
             })
-            .then(response => {
-                console.log('Fetch response:', response);
-                if (response.ok) {
-                    alert('Query executed successfully!');
-                } else {
-                    throw new Error('Query execution failed.');
-                }
-            })
-            .catch(error => {
-                console.error('There was a problem with the query execution:', error);
-            });
+                .then(response => {
+                    console.log('Fetch response:', response);
+                    if (response.ok) {
+                        alert('Query executed successfully!');
+                    } else {
+                        throw new Error('Query execution failed.');
+                    }
+                })
+                .catch(error => {
+                    console.error('There was a problem with the query execution:', error);
+                });
 
             const executeButton = document.querySelector('input[type="submit"][value="Exécuter"]') as HTMLInputElement;
             console.log('Execute button:', executeButton);
@@ -93,22 +93,22 @@ const AlumniImportExport: React.FC = () => {
     return (
         <>
             <Header />
-            <div className="container text-center my-5">
+            <div className="alumni-import-export container text-center my-5">
                 <div className="mb-5">
                     <h1 className="import-export-title" style={{ color: '#003366' }}>Export</h1>
                     <div className="row justify-content-center">
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label htmlFor="yearSelect" style={{ color: '#003366' }}>Promotion de l'année: </label>
-                                <input 
-                                    type="number" 
-                                    id="yearSelect" 
-                                    className="form-control d-inline-block w-auto mx-2" 
-                                    value={selectedYear} 
-                                    onChange={(e) => setSelectedYear(parseInt(e.target.value, 10))} 
+                                <input
+                                    type="number"
+                                    id="yearSelect"
+                                    className="form-control d-inline-block w-auto mx-2"
+                                    value={selectedYear}
+                                    onChange={(e) => setSelectedYear(parseInt(e.target.value, 10))}
                                 />
-                                <button 
-                                    onClick={handleDownload} 
+                                <button
+                                    onClick={handleDownload}
                                     className="custom-btn mr-2"
                                 >
                                     Exporter
@@ -125,22 +125,22 @@ const AlumniImportExport: React.FC = () => {
                                 <div className="form-group-row">
                                     <input
                                         type="file"
-                                        id="fileInput" 
+                                        id="fileInput"
                                         onChange={handleUpload}
                                         className="custom-file-input"
                                         accept=".sql"
                                     />
                                 </div>
                                 <label htmlFor="fileInput" style={{ color: '#003366' }}>Importer un fichier:</label>
-                                <label 
-                                    htmlFor="fileInput" 
+                                <label
+                                    htmlFor="fileInput"
                                     className="custom-file-label"
                                 >
                                     Importer
                                 </label>
                                 {fileName && <div className="file-name">{fileName}</div>}
-                                <button 
-                                    onClick={handleExecuteQuery} 
+                                <button
+                                    onClick={handleExecuteQuery}
                                     className="custom-btn custom-btn-validate mt-3"
                                 >
                                     Valider
