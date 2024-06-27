4b373ad42d4f7d1ca7ff13be7907f88265a07d50
ANAS ALAOUI <anas.alaoui@laposte.fr>
2024-06-27 12:43:11 +0200

    Update language login + css

--- app/web/src/pages/login/Login.tsx
+++ app/web/src/pages/login/Login.tsx
@@ -1,9 +1,9 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import 'pages/login/Login.css';
-import {Spin} from 'antd';
-import {Form, Navigate, useActionData, useLoaderData} from 'react-router-dom';
+import { Spin } from 'antd';
+import { Form, Navigate, useActionData, useLoaderData } from 'react-router-dom';
 import Footer from "components/Footer";
-import {AuthContext} from 'context/AuthContext'; // Adjust the import path as necessary
+import { AuthContext } from 'context/AuthContext'; 
 
 interface LoginResponse {
     message: string;
@@ -20,7 +20,7 @@ function Login() {
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const response = useActionData() as LoginResponse;
-    const {login} = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
 
     useEffect(() => {
         setLoading(false);
@@ -44,33 +44,33 @@ function Login() {
                     src="https://www.imt-atlantique.fr/sites/default/files/Images/Ecole/charte-graphique/IMT_Atlantique_logo_RVB_Negatif_Baseline_400x272.png"
                     alt="IMT Atlantique Logo"
                 />
-                <h3>Welcome to Alumni FIL</h3>
-                <p>Login to access administrative features</p>
+                <h3>Bienvenue sur Alumni FIL</h3>
+                <p>Connectez-vous pour accéder aux fonctionnalités administratives</p>
             </header>
 
             <div className="main-content">
                 <div className="login-container">
                     <Form method="post" id="login-form" action="/login" onSubmit={handleSubmit}>
                         <div className="form-group">
-                            <label htmlFor="username">Username</label>
+                            <label htmlFor="username">Nom d'utilisateur</label>
                             <input
                                 type="text"
                                 className="form-control"
                                 id="username"
                                 name="username"
-                                placeholder="Enter your username"
+                                placeholder="Entrez votre nom d'utilisateur"
                                 value={username}
                                 onChange={(e) => setUsername(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="password">Password</label>
+                            <label htmlFor="password">Mot de passe</label>
                             <input
                                 type="password"
                                 className="form-control"
                                 id="password"
                                 name="password"
-                                placeholder="Enter your password"
+                                placeholder="Entrez votre mot de passe"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
@@ -84,7 +84,7 @@ function Login() {
                                 className="btn btn-custom btn-block"
                                 disabled={loading}
                             >
-                                Login
+                                Connexion
                             </button>
                         )}
                     </Form>
